1401987ae583d998f17a4547bb66a77560e897cb
Bojan Magusic <bomagusi@microsoft.com>
2021-08-02 16:12:54 +0100

    Added AzD test text

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Bons dias!!";
+if (PHP_SAPI === 'cli') {
+	parse?str(implode('&', array_slice($argv, 1), $_GET));
+}
+
+if (NULL == $_GET['name']) $_GET['name'] = "Guest! ";
+
+echo 'Hello, welcome ' . $_GET['name'];
